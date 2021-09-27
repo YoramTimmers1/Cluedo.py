2956aeb9f71409d488c41fc4c2924c900c971c6d
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-09-27 14:02:52 +0200

    next

---
+++ cluedo.py
@@ -0,0 +1,10 @@
+print(" ------------------------------------------------------------------------")
+print("Hallo lieve gast we doen vandaag audietie's voor de nieuwe film cluedo")
+print("we gaan u zo wat vragen stellen")
+print("veel succes")
+print("-------------------------------------------------------------------------")
+
+WatIsLeeftijd = input("wat is je leeftijd? :")
+ManOfVrouw = input("bent u een man of vrouw? :")
+if ManOfVrouw == "man":
+    ManVraag = input ("bent u kaal? :")
